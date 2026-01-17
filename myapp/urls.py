4ee954f3c15119f35cbc08fa83f77f3abc483886
piyush <piyushgope77@gmail.com>
2026-01-18 04:46:32 +0530

    template completed
    here i have learned the templates.
    
    how we can use template and define the variables and use the for loop and make the custom id urls for the products and items and underatand the django template language how its works
    
    how we use the links in the urls
    
    { urls "details" item.id}
    
    and learned the namespace of the urls like
    
    app_name= 'myapp'
    
    then
    this will changes to
    { urls "myapp:details" item.id}

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'myapp'
+
 urlpatterns = [
         path('', views.index, name='index'),
+        path('<int:id>/', views.details, name ='details'),
         path('friends/', views.friends, name='friends'),
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,19 @@ from .models import Items
 # Create your views here.
 def index(request):
     data = Items.objects.all()
-    return render(request, "myapp/index.html")
+    
+    context = {
+        "Item_list" : data
+    }
+    return render(request, "myapp/index.html", context)
 
-                  
+def details(request,id):
+    item = Items.objects.get(id = id)
+    context = {
+        "item" : item
+    }
+    return render(request,'myapp/details.html', context )
+
 def friends(request):
     friends_list = ['Mohan', 'Chirag', 'Pranav', 'Abhishek']
     return HttpResponse("<h1>" + friends_list[3] + "</h1>")
