dea542bbf4f89ce579bd6858aa47e60bc2530213
piyush <piyushgope77@gmail.com>
2026-01-29 03:01:33 +0530

    Add item_image field and base template to app
    Introduced a new CharField 'item_image' to the Items model and created a corresponding migration. Added a base.html template for layout reuse and updated index.html and details.html to extend from the base template, improving structure and maintainability.

--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,4 @@ class Items(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default='500')
